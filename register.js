03547d7fe65c4d550a02006ada7e66fbc9f61611
Corentinmiserque <corentinmiserque1@gmail.com>
2024-01-16 10:57:35 +0100

    then

--- register.js
+++ register.js
@@ -1,16 +1,18 @@
 if ('serviceWorker' in navigator) {
     // Enregistrez le service worker
     navigator.serviceWorker.register('service-worker.js')
-    window.addEventListener('load', e => {
-
-    })
       .then((registration) => {
         console.log('Service Worker enregistré avec succès:', registration.scope);
       })
       .catch((error) => {
         console.error("Erreur lors de l'enregistrement du Service Worker:", error);
       });
-    }
-    else{
-        console.log("navigateur pas services workes")
-    }
+  
+    // Attendez que la page soit complètement chargée pour ajouter d'autres fonctionnalités si nécessaire
+    window.addEventListener('load', (event) => {
+      // Ajoutez d'autres fonctionnalités après le chargement de la page si nécessaire
+    });
+  } else {
+    console.log("Le navigateur ne prend pas en charge les service workers.");
+  }
+  
